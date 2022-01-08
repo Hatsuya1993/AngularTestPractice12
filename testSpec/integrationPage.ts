c249461889261439fb12af6d60188a18af222ec6
Hazrul <hal1993@hotmail.com>
2022-01-09 06:56:11 +0800

    new test

--- testSpec/integrationPage.ts
+++ testSpec/integrationPage.ts
@@ -186,9 +186,16 @@ describe('Testing orderPage', () => {
         expect(await browser.getCurrentUrl()).toContain('cars/cartrawler')
     })
     
-    fit('Should open to the correct website for kiwitaxic', async () => {
+    it('Should open to the correct website for kiwitaxic', async () => {
         let integratePage = await viewToPage.moveToIntegrate(demoPage.integrationOption)
         await Helper.clickItem(integratePage.kititaxi)
         expect((await browser.getAllWindowHandles()).length).toBe(2)
+    })
+    
+    fit('Should open to the correct website for cartrawler doc', async () => {
+        let integratePage = await viewToPage.moveToIntegrate(demoPage.integrationOption)
+        await Helper.clickItem(integratePage.kititaxi)
+        await Helper.handleTabs(1)
+        expect(await browser.getCurrentUrl()).toContain('kiwitaxi')
     })
 })
