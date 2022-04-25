94fe0857f48dbd5890d6864f779ac677a6a37de4
Hazrul <hal1993@hotmail.com>
2022-04-25 08:36:55 +0800

    new test

--- testSpec/flightReservationModulePage.ts
+++ testSpec/flightReservationModulePage.ts
@@ -77,11 +77,14 @@ describe("Flights Reservation Module", () => {
   });
 
   /* Verify option 2 description is displayed */
-  fit("Verify options 2 description is displayed", async () => {
-    expect(await flightReservationModulePage.getOptionsTwoDescription().getText()).toBe("Dedicated flights module with multiple features and functionalities such as adding flights managing routes airlines and working with multiple GDS APIs. it also supports multi-currency and multi-language with ability to auto update currency rates and support with major online payment gateways to accept payments for each booking made on your website.");
+  it("Verify options 2 description is displayed", async () => {
+    expect(await flightReservationModulePage.getOptionsTwoDescription().isDisplayed()).toBeTruthy();
   });
 
   /* Verify option 2 description is correctly populated*/
+  fit("Verify options 2 description is displayed", async () => {
+    expect(await flightReservationModulePage.getOptionsTwoDescription().getText()).toBe("Dedicated flights module with multiple features and functionalities such as adding flights managing routes airlines and working with multiple GDS APIs. it also supports multi-currency and multi-language with ability to auto update currency rates and support with major online payment gateways to accept payments for each booking made on your website.");
+  });
 
   /* Verify option 3 title is displayed */
 
